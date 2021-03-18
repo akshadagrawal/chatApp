50c3d9ca0ea7f41e47b419575d8c1f39ba8c9b02
Akshad Agrawal <akshadagrawal.aa@gmail.com>
2021-03-18 12:51:27 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { ChatEngine} from 'react-chat-engine';
-import ChatFeed from './components/ChatFeed';
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       projectID="b0affe7e-639a-418e-bc46-d30b08f6a5c2"
       userName="akshad5054"
       userSecret="Akshad@123"
-      renderChatFeed= {(chatAppProps) => <ChatFeed { ...chatAppProps} />}
     />
   );
 }
--- src/components/TheirMessage.js
+++
@@ -1,7 +0,0 @@
-const TheirMessage = () => {
-    return ( 
-        <div>My message</div>
-     );
-}
- 
-export default TheirMessage;
